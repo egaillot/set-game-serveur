06339d337821f6835fc11ddc04e27e364d0db202
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2016-11-25 16:47:07 +0100

    Le serveur sait redémarrer une partie

--- test/serverTest.js
+++ test/serverTest.js
@@ -48,6 +48,21 @@ describe("Le serveur", function () {
       done();
     });
   });
+
+  it("initialise le score à zéro au démarrage d'une partie", function (done) {
+    var idJoueurAppele = null;
+    fauxScoreur.demarrePartie = function (idJoueur) {
+      idJoueurAppele = idJoueur;
+    };
+
+    request.post("http://localhost:1789/player/12345/start", function (err, res) {
+      expect(res.statusCode).to.equal(204);
+      expect(idJoueurAppele).to.equal("12345");
+      done();
+    });
+
+
+  });
 });
 
 describe("Le scoreur", function () {
@@ -60,5 +75,12 @@ describe("Le scoreur", function () {
     scoreur = SS.creeScoreur();
     scoreur.incrementeScore("12345");
     expect(scoreur.score("12345")).to.equal("1");
+  });
+
+  it("sait redémarrer une partie", function () {
+    scoreur = SS.creeScoreur();
+    scoreur.incrementeScore("12345");
+    scoreur.demarrePartie("12345");
+    expect(scoreur.score("12345")).to.equal("0");
   });
 });
