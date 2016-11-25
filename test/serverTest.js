95b04bea2490fae5d352c9bf169b3151e7a06406
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2016-11-25 15:58:51 +0100

    REFACTOR: recycle le même port pour le serveur des tests

--- test/serverTest.js
+++ test/serverTest.js
@@ -3,28 +3,36 @@ var expect = require("expect.js"),
     SS = require("../lib/setServeur");
 
 describe("Le serveur", function () {
+  var serveur, fauxScoreur;
+
+  beforeEach(function (done) {
+    fauxScoreur = {}
+    serveur = SS.creeServeur(1789, fauxScoreur, function () {
+      done();
+    });
+  });
+
+  afterEach(function (done) {
+    serveur.eteinsToi(function () {
+      done();
+    });
+  });
+
   it("écoute sur un port", function (done) {
-    SS.creeServeur(1789, {}, function () {
-      request("http://localhost:1789/", function (error, response, body) {
-          expect(response.statusCode).to.equal(200);
-          done();
-        });
+    request("http://localhost:1789/", function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
   });
 
   it("donne le score d'un joueur", function (done) {
-    var scorer = {
-      score: function (idJoueur) {
-        if (idJoueur === "12345") {
-          return "42";
-        }
-      }
+    fauxScoreur.score = function (idJoueur) {
+      if (idJoueur === "12345") return "42";
     };
-    SS.creeServeur(2607, scorer, function () {
-      request("http://localhost:2607/player/12345/score", function (err, res, body) {
-          expect(body).to.equal("42");
-          done();
-        });
+
+    request("http://localhost:1789/player/12345/score", function (err, res, body) {
+        expect(body).to.equal("42");
+        done();
     });
   });
 });
